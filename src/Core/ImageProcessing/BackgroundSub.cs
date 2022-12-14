8caf55d102241eea0e1930f048fa51c8c1c81fa2
Tunghs <juk08843@gmail.com>
2022-12-14 23:06:26 +0900

    * 사전 배경 학습을 위한 클래스 추가

---
+++ src/Core/ImageProcessing/BackgroundSub.cs
@@ -0,0 +1,29 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ImageProcessing
+{
+    public class BackgroundSub
+    {
+        private BackgroundSubtractorMOG2 _mog;
+        private readonly string preVideoPath = @"";
+        public BackgroundSub()
+        {
+
+        }
+
+        public void PreTrain()
+        {
+
+        }
+
+        public void Apply()
+        {
+            // _mog.Apply();
+        }
+    }
+}
