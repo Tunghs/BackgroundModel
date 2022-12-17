5eff04f6cd2fa8bbb731611cb7beecd810b43c20
Tunghs <juk08843@gmail.com>
2022-12-17 23:51:47 +0900

    * Blob 관련 함수 추가

--- src/Core/ImageProcessing/BackgroundSub.cs
+++ src/Core/ImageProcessing/BackgroundSub.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OpenCvSharp.ConnectedComponents;
 
 namespace Core.ImageProcessing
 {
@@ -11,6 +12,7 @@ namespace Core.ImageProcessing
     {
         private BackgroundSubtractorMOG2 _mog;
         private readonly string preVideoPath = @"";
+        private Mat _remove = new Mat();
         public BackgroundSub()
         {
 
@@ -24,6 +26,24 @@ namespace Core.ImageProcessing
         public void Apply()
         {
             // _mog.Apply();
+        }
+
+        /// <summary>
+        /// OpenCVSharp4 버전으로 변경 필요함
+        /// </summary>
+        /// <param name="obj"></param>
+        private void RunBlobDetection(Mat obj)
+        {
+            _mog.Apply(obj, _remove);
+            //CvBlobs blobs = new CvBlobs();
+            //blobs.Label(_remove);
+            //foreach (var pair in blobs)
+            //{
+            //    CvBlob blob = pair.Value;
+            //    if (blob.Area < 100)
+            //        continue;
+            //    Cv2.Rectangle(obj, new OpenCvSharp.Point(blob.MinX, blob.MinY), new OpenCvSharp.Point(blob.MaxX, blob.MaxY), Scalar.Red, 1, LineTypes.AntiAlias);
+            //}
         }
     }
 }
